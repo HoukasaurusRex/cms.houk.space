d9e1bdd6361f86960d0a32b6c6a7f3f8505e9fee
Pterobyte <jt1992@gmail.com>
2020-03-29 18:52:22 +0800

    feat: add draft tags to articles

--- api/articles/controllers/articles.js
+++ api/articles/controllers/articles.js
@@ -1,8 +1,27 @@
-'use strict';
+'use strict'
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils')
+
+module.exports = {
+  async find(ctx) {
+
+    ctx.query = {
+      ...ctx.query,
+      status: 'published'
+    }
+
+    const entities = ctx.query._q
+      ? await strapi.services.articles.search(ctx.query)
+      : await strapi.services.articles.find(ctx.query)
+
+    return entities.map(entity =>
+      sanitizeEntity(entity, { model: strapi.models.articles })
+    )
+  },
+}
+
